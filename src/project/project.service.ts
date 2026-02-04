c3d8463f4a6b06e2daa1c1f7c262b2ceb768440f
Ali Aref <aliaref.dev@gmail.com>
2026-02-04 19:37:55 +0430

    update(module): define by self

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+  getProjectList() {
+    return 'project list form the project services 123';
+  }
+}
